138a40990bfb10b1d228bd8019f1b9925a584d3c
Sameer Khan <sameer_khan@epam.com>
2025-11-24 21:01:51 +0530

    fix skills page and improve experience page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    server: {
-        host: true
-    }
-});
+export default defineConfig({});
